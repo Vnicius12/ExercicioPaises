dd76242bd9366615b866d7367a6106990b3ee051
arqdsis <arqdsis@mocli10236.animaedu.intranet>
2018-04-23 20:13:30 -0300

    Initial commit

---
+++ app/src/main/java/br/usjt/deswebmob/aula05/MainActivity.java
@@ -0,0 +1,92 @@
+package br.usjt.deswebmob.aula05;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.Spinner;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends Activity {
+
+    Spinner spinnerContinente;
+    public static final String CHAVE = "br.usjt.desmob.geodata.txtContinente";
+    public static String JSONCONTENT;
+    private ArrayList<Pais> paises;
+    String continente = "Todos";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        spinnerContinente = (Spinner)findViewById(R.id.spinnerContinente);
+        spinnerContinente.setOnItemSelectedListener(new PaisSelecionado());
+        new ConsomeWSPaises().execute("https://restcountries.eu/rest/v2/all");
+    }
+
+    /**
+     * Autor: Vinicius Fernando M. Costa
+     * RA: 816111737
+     */
+    public void buscarPaises(View view){
+        Intent intent = new Intent(this, ListaPaisesActivity.class);
+
+        intent.putExtra(CHAVE, continente);
+        intent.putExtra("JSONRETURN", JSONCONTENT);
+
+        startActivity(intent);
+    }
+
+    private class PaisSelecionado implements AdapterView.OnItemSelectedListener {
+        @Override
+        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+            continente = (String) parent.getItemAtPosition(position);
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> parent) {
+
+        }
+    }
+
+    private class ConsomeWSPaises extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... url) {
+            OkHttpClient client = new OkHttpClient();
+
+            try{
+
+                Request request = new Request.Builder().url(url[0]).build();
+                Response response = client.newCall(request).execute();
+                String json = response.body().string();
+
+                //System.out.println(json);
+                return json;
+
+            }catch (IOException e){
+                e.printStackTrace();
+                return null;
+            }
+
+        }
+
+        @Override
+        protected void onPostExecute(String json) {
+            JSONCONTENT = json;
+        }
+
+    }
+}
